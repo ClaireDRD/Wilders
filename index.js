25d4f40a62e22688a6d90ab4dd762765f2cd2219
Claire Durand <lepretre.c@gmail.com>
2021-09-29 11:57:22 +0200

    create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const wilder1 = "Anais Bonin";
+const wilder2 = "Arnaud Didier";
+const wilder3 = "Devy Lebry";
+const wilder4 = "Francesco Saverio Viglione";
+
+const chalk = require('chalk');
+
+console.log(chalk.red.bold(wilder1));
+console.log(chalk.yellow.bold(wilder2));
+console.log(chalk.blue.bold(wilder3));
+console.log(chalk.green.bold(wilder4));
